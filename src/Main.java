0318d5e37dec3e9439fd97e9d1889e3925d360d9
Valery Poltavets <stoorman87@gmail.com>
2025-05-25 23:18:12 +0400

    version 0.0

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        char[] inDataArray = {'Т','е','к','с','т', ',' ,' ','к','о','т','о','р','ы','й',' ','н','у','ж','н','о',
+                ' ','з','а','ш','и','ф','р','о','в','а','т','ь'};
+
+
+        char[] test = EncoderDecoder.encryption(inDataArray,-3);
+        System.out.println(Arrays.toString(test));
+        test = EncoderDecoder.decryption(test,-3);
+        System.out.println(Arrays.toString(test));
+        }
+    }
